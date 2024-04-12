0f75d63c64a5b876ff8e3f03259fa994290eeb82
jirkajirka6565 <polisjiri1@seznam.cz>
2024-04-12 14:07:56 +0200

    testing serial communication with serialcom.py

--- serialcom.py
+++ serialcom.py
@@ -1,3 +1,16 @@
-import pyserial
+import serial #import pyserial
+import time #import time library
 
 arduino = serial.Serial(port="/dev/ttyACM0", baudrate=9600, timeout=1)
+
+def TurnLedOn():
+    arduino.write(b"1")
+
+def TurnLedOff():
+    arduino.write(b"0")
+
+while(True):
+    TurnLedOn()
+    time.sleep(1)
+    TurnLedOff()
+    time.sleep(1)
