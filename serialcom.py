fdd2430f8e2d713df36d1632bfb29872c54277d3
jirkajirka6565 <polisjiri1@seznam.cz>
2024-04-13 13:09:28 +0200

    serialcom.py led on/off testing

--- serialcom.py
+++ serialcom.py
@@ -1,16 +1,25 @@
 import serial #import pyserial
 import time #import time library
+import tkinter as tk #import tkinter library
 
-arduino = serial.Serial(port="/dev/ttyACM0", baudrate=9600, timeout=1)
+root = tk.Tk() #initialize tkinter
+arduino = serial.Serial(port="/dev/ttyACM0", baudrate=9600, timeout=1) #select arduino port
 
+#define function for led on
 def TurnLedOn():
     arduino.write(b"1")
+    print(arduino.readline().decode("utf-8"))
 
+#define function for led off
 def TurnLedOff():
     arduino.write(b"0")
+    print(arduino.readline().decode("utf-8"))
 
-while(True):
-    TurnLedOn()
-    time.sleep(1)
-    TurnLedOff()
-    time.sleep(1)
+#buttons for Led on/off
+LedOnButton = tk.Button(root, text="Led On", command=TurnLedOn)
+LedOnButton.pack()
+LedOffButton = tk.Button(root, text="Led Off", command=TurnLedOff)
+LedOffButton.pack()
+
+
+root.mainloop()
