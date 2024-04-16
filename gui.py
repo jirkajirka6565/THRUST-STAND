a92c8926606536664daf5f58517a9a7794d0316a
jirkajirka6565 <polisjiri1@seznam.cz>
2024-04-16 13:26:42 +0200

    minor changes in gui.py

--- gui.py
+++ gui.py
@@ -1,18 +1,10 @@
 import serial #import pyserial
 import serial.tools.list_ports
 import time #import time library
-
-#tkinter designer imports
-from pathlib import Path
-from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
+import tkinter
+from tkinter import *
 
 
-OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\polis\Documents\Coding\Tkinter-Designer-1.0.7\build\assets\frame0")
-
-
-def relative_to_assets(path: str) -> Path:
-    return ASSETS_PATH / Path(path)
 
 def get_ports():
     ports = serial.tools.list_ports.comports()
@@ -20,18 +12,25 @@ def get_ports():
 
 def FindArduino(portsFound):
     comport = "no port"
+    print("\n")
     numOfPorts = len(portsFound)
 
     for i in range(0, numOfPorts):
         port = portsFound[i]
         strPort = str(port)
+        print(strPort)
+        print("\n")
 
         if "Arduino" in strPort:
             splitPort = strPort.split(" ")
             comport = splitPort[0]
+            print(comport)
+        elif "ACM0" in strPort:
+            splitPort = strPort.split(" ")
+            print(splitPort)
+            comport = splitPort[0]
 
     return comport
-
 
 arduino_port = FindArduino(get_ports())
 print(arduino_port)
@@ -41,7 +40,6 @@ try:
     arduino = serial.Serial(port=arduino_port, baudrate=9600, timeout=1) #select arduino port
 except:
     print("Port not found")
-    exit()
 
 #define function for led on
 def TurnLedOn():
@@ -70,16 +68,13 @@ canvas = Canvas(
 )
 
 canvas.place(x = 0, y = 0)
-image_image_1 = PhotoImage(
-    file=relative_to_assets("image_1.png"))
-image_1 = canvas.create_image(
-    379.0,
-    360.0,
-    image=image_image_1
-)
+
+image_image_1 = PhotoImage(file="image_1.png")
+
+canvas.create_image(379.0, 360.0, image=image_image_1)
 
 button_image_1 = PhotoImage(
-    file=relative_to_assets("button_1.png"))
+    file="button_1.png")
 button_1 = Button(
     image=button_image_1,
     borderwidth=0,
@@ -95,7 +90,7 @@ button_1.place(
 )
 
 button_image_2 = PhotoImage(
-    file=relative_to_assets("button_2.png"))
+    file="button_2.png")
 button_2 = Button(
     image=button_image_2,
     borderwidth=0,
@@ -109,5 +104,5 @@ button_2.place(
     width=236.0,
     height=96.0
 )
-window.resizable(False, False)
+
 window.mainloop()
